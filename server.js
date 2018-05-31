2378a575082a906cf1e042548ace5c0d7d2c38e9
puchk <kpuchalski@gmail.com>
2018-05-31 14:00:17 -0700

    sign in working with hashed pw

--- server.js
+++ server.js
@@ -50,11 +50,22 @@ app.get('/', (req, res) => {
 });
 
 app.post('/signin', (req, res) => {
-	if (req.body.email === db.users[0].email && req.body.password === db.users[0].password) {
-		res.json(db.users[0]);
-	} else {
-		res.status(400).json('error');
-	}
+	knex.select('email', 'hash').from('login')
+		.where('email', '=', req.body.email)
+		.then(data => {
+			const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+			if (isValid) {
+				return knex.select('*').from('users')
+					.where('email', '=', req.body.email)
+					.then(user => {
+						res.json(user[0])
+					})
+					.catch(err => res.status(400).json('unable to get user'))
+			} else {
+				res.status(400).json('wrong info')
+			}
+		})
+		.catch(err => res.status(400).json('wrong information'))
 });
 
 app.post('/register', (req, res) => {
