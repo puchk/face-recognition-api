1ae75f27a9de4e212e8b5d1d9f2fef48c683ee01
puchk <kpuchalski@gmail.com>
2018-06-07 23:15:30 -0700

    db fixes

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 const knex = require('knex')({
   client: 'pg',
   connection: {
-    host : process.env.DATABASE_URL,
+    connectionString : process.env.DATABASE_URL,
     ssl: true
   }
 });
